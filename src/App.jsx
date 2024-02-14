b01d720d624855d358e6a7f6eee4e0ba4b425a16
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-02-14 15:56:28 +0300

    js-jsx farkı

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Hello from "./Hello";
 import Login from "./Login";
 import { users } from "./Login";
 
@@ -7,11 +8,8 @@ function App() {
   return (
     <div>
       <Login />
-      <hr />
-      <Login />
-      <hr />
-      <Login />
-      <hr />
+
+      <Hello />
     </div>
   );
 }
@@ -24,6 +22,10 @@ Oluşturduğumuz componenti istediğimiz kadar başka yerde kullanabiliriz.
 export default ile componentin içindeki herşeye erişiyoruz
 export diyerek sadece o componente ait bir parçaya erişebiliyoruz.
 vexport default ettiğimiz componenti normal yazarak sadece export ettiğimiz componenti ise süslü parantez içinde import etmemiz gerekir .
+-----------------------------------------------
 
+Js-Jsx Arasındaki Fark :
+jsx ile direk html in içinden javascript kodlarına erişip kolayca işlemimizi yaparken 
+js ile yaptığımızda kök dizinde return React.createElement("div" , null , "Hello world") diyerk uğraşmamız gerek
 
 */
